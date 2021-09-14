1520f7ae787841e75b4f922430e93639e813af8d
JDu8Du8 <87732517+JDu8Du8@users.noreply.github.com>
2021-09-14 14:21:46 -0700

    Update SQL Week 2 CHW.sql

--- SQL Week 2 CHW.sql
+++ SQL Week 2 CHW.sql
@@ -3,4 +3,4 @@ select t.title as "Title", count(title) as "Employee Count" from employees e joi
 -- #2 Average salary per title.
 select t.title as "Title", truncate(avg(s.salary),2) as "Average Salary" from titles t join salaries s on t.emp_no = s.emp_no group by t.title;
 -- #3 Sum of Salaries for Marketing between years 1990 and 1992.
-select sum(s.salary) as "1990 - 1992 Total Salaries" from salaries s  join dept_emp d on d.emp_no = s.emp_no where d.from_date between '1990-01-01' and '1992-12-31' and d.dept_no  = 'd001' group by d.dept_no;
+select sum(s.salary) as "Total Salaries between 1990 and 1992" from salaries s  join dept_emp d on d.emp_no = s.emp_no where d.from_date between '1990-01-01' and '1991-12-31' and d.dept_no  = 'd001' group by d.dept_no;
